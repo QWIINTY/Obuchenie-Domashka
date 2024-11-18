ad68778e54916579dcd54ff0eee9837f074aebce
QWINTYT <123218398+QWIINTY@users.noreply.github.com>
2024-11-18 23:15:41 +0300

    Решил дз по сортировке.

---
+++ dz3.5/dz3.5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz3._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10] { 9, 3, 5, 1, 2, 4, 7, 8, 6, 10 };
+            int tempNumber;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        tempNumber = array[j];
+                        array[j] = array[i];
+                        array[i] = tempNumber;
+
+                    }
+                }
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i] + " | ");
+            }
+        }
+    }
+}
