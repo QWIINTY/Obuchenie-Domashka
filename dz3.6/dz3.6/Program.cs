0458b60abd2f9b6a4acce7f9d5bbd1a3334b47e4
QWINTYT <123218398+QWIINTY@users.noreply.github.com>
2024-11-20 00:59:28 +0300

    Dz Split complite!

---
+++ dz3.6/dz3.6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz3._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Бу! испугался? не бойся. я добрый.";
+
+            char[] separators=new char[] { ' ', '.', '!', '?' };
+            string[]wordsSplit=text.Split(separators,StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string word in wordsSplit)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
