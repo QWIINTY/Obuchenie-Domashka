f2da6c683cc298b846147a3ad342d288d606a8b2
QWINTYT <123218398+QWIINTY@users.noreply.github.com>
2024-11-26 21:32:18 +0300

    Complite dz

---
+++ dz3.8/dz3.8/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz3._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] brackets = new string[8] { "(", "(", ")", "(", "(", ")", ")", ")" };
+            int depth = 0;
+            int counter = 0;
+
+            for (int i = 0; i < brackets.Length; i++)
+            {
+                if (brackets[i] == "(")
+                {
+                    counter++;
+                }
+                else if (brackets[i] == ")")
+                {
+                    if (depth < counter)
+                    {
+                        depth = counter;
+                    }
+                    counter--;
+                }
+                if (counter < 0)
+                {
+                    Console.WriteLine("Нехорошее выражение!");
+                    break;
+                }
+            }
+            if (counter == 0)
+            {
+                Console.WriteLine($"Намана! Глубина - {depth}");
+            }
+            else if (counter > 0)
+            {
+                Console.WriteLine("Нехорошее выражение!");
+            }
+        }
+    }
+}
