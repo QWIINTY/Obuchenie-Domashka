564435f96c7985fa52070c0c95843640d59a6868
QWINTYT <123218398+QWIINTY@users.noreply.github.com>
2024-12-07 14:25:20 +0300

    Reshil dz4.3

---
+++ dz4.3/dz4.3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz4._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayNumbers = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            for (int i = 0; i < arrayNumbers.Length; i++)
+            {
+                Console.Write($"| {arrayNumbers[i]} |");
+            }
+            Console.WriteLine(" - Original");
+
+            Shuffle(arrayNumbers);
+        }
+
+        static void Shuffle(int[] array)
+        {
+            Random random = new Random();
+         
+            int randomIndex;
+            int shuffledElement;
+
+            for (int i = array.Length-1; i >=0; i--)
+            {
+                randomIndex=random.Next(i);
+                shuffledElement = array[randomIndex];
+                array[randomIndex] = array[i];
+                array[i] = shuffledElement;
+                Console.Write($"| {shuffledElement} |");
+            }
+        Console.WriteLine(" - Shuffle");
+        }
+        }
+
+}
+
+
